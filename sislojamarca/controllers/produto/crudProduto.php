a477a9ce055e27e64663a82000ac8421588366d1
bruno-rodrigues0 <brunorodriguesmtv0@gmail.com>
2024-08-12 15:24:00 -0300

    alterar produto funcionando

--- sislojamarca/controllers/produto/crudProduto.php
+++ sislojamarca/controllers/produto/crudProduto.php
@@ -23,3 +23,27 @@ function findAllProduto() {
         return $e->getMessage();
     }
 }
+
+function updateProduto($cbarra, $titulo, $preco, $marca, $tipo, $detalhesproduto, $tamanhos) {
+    require realpath(__DIR__ . "/../../database/conexao.php");
+    $sql = "UPDATE produto SET titulo = '$titulo', preco = '$preco', marca = '$marca', tipo = '$tipo', detalhesProduto = '$detalhesproduto', tamanhos = '$tamanhos' WHERE CBarra = '$cbarra'";
+    $stm = $conn->prepare($sql);
+    try {
+        $stm->execute();
+        return $stm->fetch();
+    } catch (PDOException $e) {
+        return $e->getMessage();
+    }
+}
+
+function findProdutoByCBarra($cbarra) {
+    require realpath(__DIR__ . "/../../database/conexao.php");
+    $sql = "SELECT * FROM produto WHERE CBarra = '$cbarra'";
+    $stm = $conn->prepare($sql);
+    try {
+        $stm->execute();
+        return $stm->fetch();
+    } catch (PDOException $e) {
+        return $e->getMessage();
+    }
+}
--- sislojamarca/php/altproduto.php
+++ sislojamarca/php/altproduto.php
@@ -1,3 +1,39 @@
+<?php 
+session_start();
+
+require_once realpath(__DIR__ . "/../controllers/produto/crudProduto.php");
+require_once realpath(__DIR__ . "/../controllers/marca/crudMarca.php");
+
+$list_produtos = findAllProduto();
+$marcas = findAllMarca();
+$produto_search = [];
+$cbarra = "";
+
+if(isset($_GET["Cbarra"])){
+    $cbarra = $_GET["Cbarra"];
+    $produto_search = findProdutoByCBarra($cbarra);
+
+    if(isset($_POST["update"])){
+        $titulo = $_POST["titulo"];
+        $preco = $_POST["preco"];
+        $marca = $_POST["marcas"];
+        $tipo = $_POST["tipo"];
+        $detalhesproduto = $_POST["detalhesProduto"];
+        $tamanhos = $_POST["tamanhos"];
+        
+        $result = updateProduto($cbarra, $titulo, $preco, $marca, $tipo, $detalhesproduto, $tamanhos);
+        
+        if($result){
+            echo "Produto Alterado com Sucesso!";
+        } else {
+            echo "Erro ao Alterar Produto!";
+        }
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -19,43 +55,51 @@
    
     <main>
         <div class="cadastro">
-            <form action="todosprodutos.php" method="post">
-
-            <label for="Cbarra"><p>C. Barra</p></label>
-                    <input type="text" name="Cbarra" id="Cbarra" value="<?php echo $CBarra ?>" required><br>
-
+            <form action="" method="get" class="cbarra-form">
+                <input type="hidden" name="cbarra">
+                <label for="Cbarra"><p>C. Barra</p></label>
+                <select name="Cbarra" id="Cbarra" onchange="submit()">
+                    <option value="NA">N\A</option>
+                    <?php foreach ($list_produtos as $produto) { ?>
+                        <option value="<?= $produto["CBarra"]?>" <?php if($produto["CBarra"] == $_GET["Cbarra"]) echo "selected='selected'"?>><?= $produto["CBarra"]?></option>
+                    <?php } ?>
+                </select>
+            </form>
+            <form action="" method="post">
                 <label for="titulo"><p>Titulo</p></label>
-                    <input type="text" name="titulo" id="titulo" value="<?php echo $titulo ?>" required><br>
+                    <input type="text" name="titulo" id="titulo" value="<?= $produto_search["titulo"]?>" required><br>
 
                 <label for="preco"><p>Preço</p></label>
-                    <input type="number" name="preco" id="preco" value="<?php echo $preco ?>" required><br>
+                    <input type="number" name="preco" id="preco" value="<?= $produto_search["preco"]?>" required><br>
 
                 <label for="marcas"><p>Marcas</p></label>
                     <select id="marcas" name="marcas">
-                        <option value="ESE">ESE</option>
-                        <option value="LAB">LABORATORIA</option>
-                        <option value="NE">NOVA ERA</option>
+                        <?php foreach($marcas as $marca) {?>
+                            <option value="<?= $marca["IDMarca"]?>"><?= $marca["nomeMarca"]?></option>
+                        <?php } ?>
                     </select><br>
 
                 <label for="tipo"><p>Tipos</p></label>
+
+                
                     <select id="tipo" name="tipo">
-                        <option value="C">Camisa</option>
-                        <option value="M">Moletom</option>
-                        <option value="R">Regata</option>
+                        <option value="C" <?php if($produto_search["tipo"] == "C") echo "selected='selected'"?>>Camisa</option>
+                        <option value="M" <?php if($produto_search["tipo"] == "M") echo "selected='selected'"?>>Moletom</option>
+                        <option value="R" <?php if($produto_search["tipo"] == "R") echo "selected='selected'"?>>Regata</option>
                     </select><br>
 
                 <label for="detalhesProduto"><p>Detalhes do Produto</p></label>
-                    <input type="tel" name="detalhesProduto" id="detalhesProduto" value="<?php echo $detalhesProduto ?>"><br>
+                    <input type="tel" name="detalhesProduto" id="detalhesProduto" value="<?= $produto_search["detalhesProduto"]?>" ><br>
 
                 <label for="tamanhos"><p>Tamanhos</p></label>
                     <select id="tamanhos" name="tamanhos">
-                        <option value="P">P</option>
-                        <option value="M">M</option>
-                        <option value="G">G</option>
-                        <option value="GG">GG</option>
+                        <option value="P" <?php if($produto_search["tamanhos"] == "P") echo "selected='selected'"?>>P</option>
+                        <option value="M" <?php if($produto_search["tamanhos"] == "P") echo "selected='selected'"?>>M</option>
+                        <option value="G" <?php if($produto_search["tamanhos"] == "P") echo "selected='selected'"?>>G</option>
+                        <option value="GG" <?php if($produto_search["tamanhos"] == "P") echo "selected='selected'"?>>GG</option>
                     </select><br>
 
-                <input type="submit" value="Cadastrar"><br><br>
+                <input type="submit" value="Alterar" name="update"><br><br>
 
             </form>
         </div>
@@ -63,8 +107,17 @@
 
     <footer>
         <div class="voltar">
-            <a href="index.html">Voltar para a pagina Incial</a>
+            <a href="./index.php">Voltar para a pagina Incial</a>
         </div>
     </footer>
+
+    <script>
+
+        function submit(){
+            setInterval(() => {
+                document.querySelector(".cbarra-form").submit();
+            }, 1000);
+        }
+    </script>
 </body>
 </html>
